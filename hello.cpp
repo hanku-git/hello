89f90fe3fc6d4975fbbac7dd586ee785e84daac5
kshan <hanku77@gmail.com>
2026-06-23 08:37:16 +0900

    mul is added

--- hello.cpp
+++ hello.cpp
@@ -10,15 +10,23 @@ int sub(int a, int b)
     return a-b;
 }
 
+int mul(int a, int b)
+{
+    return a*b;
+}
+
 
 int main() 
 {
-    int total = 0;
+    int add_test = 0;
     int sub_test = 0;
+    int  mul_test = 0;
+    
     std::cout << "Hello, World!" << std::endl;
 
-    total = add(1,2);
+    add_test = add(1,2);
     sub_test = sub(2,1);
+    mul_test = mul(2,1);
     
     return 0;
 }
