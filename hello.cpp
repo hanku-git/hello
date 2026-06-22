b459a14f7a3f1760f16198e6ffaed7492818c37f
kshan <hanku77@gmail.com>
2026-06-22 14:40:40 +0900

    sub func is added

--- hello.cpp
+++ hello.cpp
@@ -5,12 +5,20 @@ int add(int a, int b)
     return a+b;
 }
 
+int sub(int a, int b)
+{
+    return a-b;
+}
+
+
 int main() 
 {
     int total = 0;
+    int sub_test = 0;
     std::cout << "Hello, World!" << std::endl;
 
     total = add(1,2);
+    sub_test = sub(2,1);
     
     return 0;
 }
