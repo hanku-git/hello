c31b357bfc3bdbd767cbde8b7af75e8c8b212236
kshan <hanku77@gmail.com>
2026-06-22 14:35:24 +0900

    기능 추가:add func

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main() {
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int main() 
+{
+    int total = 0;
     std::cout << "Hello, World!" << std::endl;
+
+    total = add(1,2);
+    
     return 0;
 }
