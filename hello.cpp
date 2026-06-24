0966ae86a189a31a8013b3461a73ff4da5635656
kshan <hanku77@gmail.com>
2026-06-24 09:32:27 +0900

    Revert "insert ex2"
    This reverts commit 52407d4c95339e216aff740dc7bfccdbfb570364.

--- hello.cpp
+++ hello.cpp
@@ -5,11 +5,6 @@ int insert_ex1(int a)
     return a;
 }
 
-int insert_ex2(int a)
-{
-    return a;
-}
-
 int main() 
 {
     int e1;
@@ -17,7 +12,6 @@ int main()
     std::cout << "Hello, World!" << std::endl;
 
     e1 = insert_ex1(1);
-    e1 = insert_ex2(1);
     
     return e1;
 }
