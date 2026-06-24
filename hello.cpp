52407d4c95339e216aff740dc7bfccdbfb570364
kshan <hanku77@gmail.com>
2026-06-24 09:31:41 +0900

    insert ex2

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,11 @@ int insert_ex1(int a)
     return a;
 }
 
+int insert_ex2(int a)
+{
+    return a;
+}
+
 int main() 
 {
     int e1;
@@ -12,6 +17,7 @@ int main()
     std::cout << "Hello, World!" << std::endl;
 
     e1 = insert_ex1(1);
+    e1 = insert_ex2(1);
     
     return e1;
 }
