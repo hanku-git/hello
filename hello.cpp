08440d21f54f8f25bc4445fdb56f44ee03b833c0
kshan <hanku77@gmail.com>
2026-06-24 10:31:33 +0900

    ex3 insert

--- hello.cpp
+++ hello.cpp
@@ -10,6 +10,11 @@ int insert_ex2(int a)
     return a;
 }
 
+int insert_ex3(int a)
+{
+    return a;
+}
+
 int main() 
 {
     int e1;
@@ -18,6 +23,7 @@ int main()
 
     e1 = insert_ex1(1);
     e1 = insert_ex2(1);
+    e1 = insert_ex3(1);
     
     return e1;
 }
